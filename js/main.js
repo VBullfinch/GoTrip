98c7450bdcd871db882b0fc2f996f77a267e9abd
VBullfinch <54802105+VBullfinch@users.noreply.github.com>
2022-07-25 10:01:13 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//carusel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    center:true,
+    loop:true,
+    margin:30,
+    startPosition:0,
+    items:1,
+    responsive:{
+        540:{
+            items:3,
+            startPosition:1,
+        },
+
+        1200:{
+            items:3,
+            margin:30,
+        }
+    }
+});
+$('.slider_btn--prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+})
+$('.slider_btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
+//nav icon
+const navBtn=document.querySelector('.nav_toggle');
+const nav=document.querySelector('.nav');
+const menuIcon=document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function(){
+    nav.classList.toggle('nav--mobile')
+    menuIcon.classList.toggle('menu-icon--active');
+}
